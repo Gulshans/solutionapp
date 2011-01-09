351ccf353c3bb9af8922a801a83b30f371a83a44
Gulshan <gulshansolution@gmail.com>
2011-01-08 23:20:40 -0500

    Intial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solutionapp::Application.config.secret_token = 'cd01371466efe47e422457ba4515bb2138c017493e5ebf53d9bffd9e734456952c8d1a8ec9bb12ee79204898a06478a07cb163206801d217e37ec7e28c75f424'
